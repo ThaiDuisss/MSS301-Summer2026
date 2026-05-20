6d137b808dc0524eba43f0f4b4491f7818b48d65
ThaiDuisss <thai110504@gmail.com>
2026-05-20 10:17:45 +0700

    build prj skeleton

---
+++ CV-Parser-Service/src/test/java/com/example/cvparserservice/CvParserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cvparserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvParserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Candidate-Service/src/test/java/com/example/candidateservice/CandidateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.candidateservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
